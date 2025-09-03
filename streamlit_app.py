145d57ecb79a91ec408be82e8a7e1990574d772c
elizabeth-dyson <elizabethdyson810@gmail.com>
2025-09-03 11:53:50 -0500

    added initial sidebar

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,7 +11,7 @@ def load_data(data_type: str):
 def clean_state_name(tuition_cost: pd.DataFrame):
     state_dict = {
         'AS': 'American Samoa',
-        'DC': 'Washington DC',
+        'DC': 'District of Columbia',
         'PR': 'Puerto Rico',
         'GU': 'Guam',
         'VI': 'Virgin Islands'
@@ -30,7 +30,33 @@ def get_df1(tuition_cost: pd.DataFrame, salary_potential: pd.DataFrame, diversit
 
     return df1
 
-def produce_plot1():
+def set_regions_divisions(df1: pd.DataFrame):
+    region_dict = {
+        'Northeast': ['New England', 'Middle Atlantic'],
+        'Midwest': ['East North Central', 'West North Central'],
+        'South': ['South Atlantic', 'East South Central', 'West South Central'],
+        'West': ['Mountain', 'Pacific'],
+        'Territories': ['Territories']
+    }
+    division_dict = {
+        'New England': ['Connecticut', 'Maine', 'Massachusetts', 'New Hampshire', 'Rhode Island', 'Vermont'],
+        'Middle Atlantic': ['New Jersey', 'New York', 'Pennsylvania'],
+        'East North Central': ['Illinois', 'Indiana', 'Michigan', 'Ohio', 'Wisconsin'],
+        'West North Central': ['Iowa', 'Kansas', 'Minnesota', 'Missouri', 'Nebraska', 'North Dakota', 'South Dakota'],
+        'South Atlantic': ['Delaware', 'District of Columbia', 'Florida', 'Georgia', 'Maryland', 'North Carolina', 'South Carolina', 'Virginia', 'West Virginia'],
+        'East South Central': ['Alabama', 'Kentucky', 'Mississippi', 'Tennessee'],
+        'West South Central': ['Arkansas', 'Louisiana', 'Oklahoma', 'Texas'],
+        'Moutain': ['Arizona', 'Colorado', 'Idaho', 'Montana', 'Nevada', 'New Mexico', 'Utah', 'Wyoming'],
+        'Pacific': ['Alaska', 'California', 'Hawaii', 'Oregon', 'Washington'],
+        'Territories': ['American Samoa', 'Puerto Rico', 'Guam', 'Virgin Islands']
+    }
+
+    df1['division'] = df1['state'].map(division_dict)
+    df1['region'] = df1['division'].map(region_dict)
+
+    return df1
+
+def produce_plot1(color_col: str, x_col: str, y_col: str):
     data_types = {'tc': 'tuition_cost', 'ti': 'tuition_income', 'sp': 'salary_potential', 'ht': 'historical_tuition', 'ds': 'diversity_school'}
 
     tuition_cost = load_data(data_types['tc'])
@@ -39,11 +65,27 @@ def produce_plot1():
     diversity_school = load_data(data_types['ds'])
 
     df1 = get_df1(tuition_cost, salary_potential, diversity_school)
+    df1 = set_regions_divisions(df1)
+
+    x_dict = {
+        'In-State': 'in_state_tuition',
+        'Out-of-State': 'out_of_state_tution'
+    }
+    y_dict = {
+        'Mid-Career': 'mid_career_pay',
+        'Early Career': 'early_career_pay'
+    }
+    color_dict = {
+        'State': 'state',
+        'Degree Length': 'degree_length',
+        'Region': 'region',
+        'Regional Division': 'division'
+    }
 
     fig = px.scatter(
-        df1, x='out_of_state_tuition', y='mid_career_pay', color='state', size='total_enrollment', facet_col='type',
-        labels={"in_state_tuition": "In-State Tuition", 'out_of_state_tuition': 'Out-of-State Tuition', "mid_career_pay": "Mid Career Salary", 
-                'state': 'State', 'total_enrollment': 'Total Enrollment', 'type': 'Type', 'name': 'School'},
+        df1, x=x_dict[x_col], y=y_dict[y_col], color=color_dict[color_col], size='total_enrollment', facet_col='type',
+        labels={"in_state_tuition": "In-State Tuition", 'out_of_state_tuition': 'Out-of-State Tuition', "mid_career_pay": "Mid Career Salary", 'region': 'Region',
+                'state': 'State', 'total_enrollment': 'Total Enrollment', 'type': 'Type', 'name': 'School', 'early_career_pay': 'Early Career Salary', 'division': 'Regional Division'},
         hover_name='name'
     )
     fig.update_xaxes(matches=None)
@@ -52,9 +94,23 @@ def produce_plot1():
 
     return fig
 
-fig = produce_plot1()
-
-st.write("Tuition Cost Impact on Mid-Career Salaries")
+title = st.header("Tuition Cost Impact on Mid-Career Salaries")
 
 st.set_page_config(layout="wide")
-st.plotly_chart(fig, use_container_width=True)
+
+add_color_checkbox = st.sidebar.selectbox(
+    'Color By:', ('State', 'Degree Length', 'Region', 'Regional Division')
+)
+
+add_x_checkbox = st.sidebar.selectbox(
+    'Tuition Type:', ('In-State', 'Out-of-State')
+)
+
+add_y_checkbox = st.sidebar.selectbox(
+    'Salary Type:', ('Mid-Career', 'Early Career')
+)
+
+fig1 = produce_plot1(add_color_checkbox, add_x_checkbox, add_y_checkbox)
+
+chart1 = st.plotly_chart(fig1, use_container_width=True)
+
