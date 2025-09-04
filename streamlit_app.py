870485e47c58ec7bbe41c2e04e956487210d3abe
elizabeth-dyson <elizabethdyson810@gmail.com>
2025-09-04 12:11:59 -0500

    forgot to actually add page to nav

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,8 +5,9 @@ import plotly.express as px
 
 salary_page = st.Page("tuition_salary.py", title="Salary v Tuition", icon=":material/attach_money:")
 income_level_page = st.Page("tuition_income_level.py", title="Tuition Cost by Income Level", icon=":material/attach_money:")
+diversity_page = st.Page("diversity.py", title="Diversity Statistics", icon=":material/diversity_1:")
 
-pg = st.navigation([salary_page, income_level_page])
+pg = st.navigation([salary_page, income_level_page, diversity_page])
 st.set_page_config(page_title="Tuition Data Viewer", page_icon=":material/school:", layout="wide")
 
 pg.run()
